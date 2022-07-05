f14b0699298535e6bed886f1e1a0b89f185a4578
Beckham 45 <o.becks45@gmail.com>
2022-07-05 11:47:44 +0300

    Updates

--- components/onboarding/GetStartedForm.js
+++ components/onboarding/GetStartedForm.js
@@ -90,8 +90,8 @@ const GetStartedForm = (props) => {
         </h2>
       </div>
       <div className="">
-        <div className="container mx-auto">
-          <div className="max-w-md mx-auto my-10">
+        <div className="container md:mx-auto">
+          <div className="md:max-w-md mx-auto my-10">
             <div className="text-center">
               <h1 className="my-3 text-3xl font-semibold  dark:text-gray-200">
                 Get Started
