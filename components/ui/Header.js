60c74988698811eeeeb7944ed9a9387f3dd5cec0
Beckham 45 <o.becks45@gmail.com>
2022-07-05 08:45:26 +0300

    Updates

--- components/ui/Header.js
+++ components/ui/Header.js
@@ -4,8 +4,11 @@ import Link from "next/link";
 import React from "react";
 import KosseLogo from "../../images/kosselogo.png";
 import Image from "next/image";
+import { useState } from "react";
+import menuIcon from "../../images/menuicon.png";
 
 function Header() {
+  const [showMenu, setShowmenu] = useState(false);
   return (
     <header className="flex flex-col md:w-full xl:flex xl:flex-row sticky xl:w-3/4 top-5 z-50 space-x-20  justify-start  p-5 md:px-10 md:mx-auto bg-gray-100">
       <div className="mx-auto flex items-center space-x-5">
@@ -19,6 +22,7 @@ function Header() {
             />
           </div>
         </Link>
+
         <nav>
           <div className="hidden items-center space-x-5 md:inline-flex">
             <Link href="/getstarted">
@@ -37,6 +41,43 @@ function Header() {
                 About
               </a>
             </Link>
+          </div>
+
+          <div className="sm:hidden relative">
+            <div>
+              <button
+                className="relative h-[50px] w-[50px] rounded-md "
+                onClick={() => setShowmenu(!showMenu)}
+              >
+                <Image
+                  src={menuIcon}
+                  layout="fill"
+                  objectFit="cover"
+                  className="rounded-md"
+                />
+              </button>
+            </div>
+            {showMenu && (
+              <div className="flex flex-col space-y-2 absolute top-10 righ-0 z-50 bg-gray-100 shadow-xl rounded-md p-2">
+                <Link href="/getstarted">
+                  <a className="transition-color w-full items-center justify-center rounded-md px-3 py-2 text-black duration-200 hover:bg-gray-400 hover:text-white hover:shadow-md lg:inline-flex lg:w-auto">
+                    Get Started
+                  </a>
+                </Link>
+                <Link href="/learnmore">
+                  <a className="transition-color w-full items-center justify-center rounded-md px-3 py-2 text-black duration-200 hover:bg-gray-400 hover:text-white hover:shadow-md lg:inline-flex lg:w-auto">
+                    Learn More
+                  </a>
+                </Link>
+
+                <Link href="/about">
+                  <a className="transition-color w-full items-center justify-center rounded-md px-3 py-2 text-black duration-200 hover:bg-gray-400 hover:text-white hover:shadow-md lg:inline-flex lg:w-auto">
+                    About
+                  </a>
+                </Link>
+                <div />
+              </div>
+            )}
           </div>
         </nav>
       </div>
