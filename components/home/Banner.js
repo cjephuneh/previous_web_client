a6ca9386b8f10d483d72ebdcca8d05055cf1120a
Beckham 45 <o.becks45@gmail.com>
2022-07-05 11:23:11 +0300

    Updates

--- components/home/Banner.js
+++ components/home/Banner.js
@@ -82,7 +82,7 @@ function Banner() {
       <div className="p-4 md:hidden">
         <ReactPlayer
           url="https://youtu.be/5lXbzzKvA8U"
-          width="450px"
+          width="400px"
           height="400px"
           controls={true}
         />
