2909cfb9509d2d2546a697dc94db6463da5b565e
Beckham 45 <o.becks45@gmail.com>
2022-07-06 08:05:29 +0300

    Updates

--- components/home/IndustryCard.js
+++ components/home/IndustryCard.js
@@ -15,21 +15,19 @@ function IndustryCard() {
       <div className=" py-8">
         <h1 className="font-serif text-4xl md:text-6xl p-4">Diversity</h1>
         <div className="flex flex-col p-2 space-y-4 items-center md:flex-row md:justify-center md:space-x-4">
-          <div className=" bg-gray-200 w-[250px] h-[80px] shadow-lg">
+          <div className="  w-[250px] h-[80px] shadow-lg">
             <p className="p-1">
-              user query: "What flights are available from pittsburgh to
-              baltimore on thursday morning"
+              Diversity means interacting With your customers efficiently.
             </p>
           </div>
-          <div className=" bg-gray-200 w-[250px] h-[80px] shadow-lg">
+          <div className="  w-[250px] h-[80px] shadow-lg">
             <p className="p-1">
-              user query: "Can I be able to book that hotel ?"
+              It also means interacting with your suppliers and other key
+              players.
             </p>
           </div>
-          <div className=" bg-gray-200 w-[250px] h-[80px] shadow-lg">
-            <p className="p-1">
-              user query: "Will I be able to easily use your product ?"
-            </p>
+          <div className="  w-[250px] h-[80px] shadow-lg">
+            <p className="p-1">Above all, inclusivity in diversity.</p>
           </div>
         </div>
       </div>
