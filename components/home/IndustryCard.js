85cdba3b7fff34aa82e2418630db993b9da25646
Beckham 45 <o.becks45@gmail.com>
2022-07-06 08:35:31 +0300

    Updates

--- components/home/IndustryCard.js
+++ components/home/IndustryCard.js
@@ -4,7 +4,7 @@ import React from "react";
 function IndustryCard() {
   return (
     <div className="divide-y divide-gray-400">
-      <div className=" py-8 w-3/4 md:w-1/2">
+      <div className=" py-8 w-3/4 ">
         <h1 className="font-serif text-4xl md:text-6xl p-4">
           Building for all{" "}
           <span className="underline decoration-black decoration-4">
