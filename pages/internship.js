26547f45aa74a53fa97e2fcaa431b6cbea5524da
Beckham 45 <o.becks45@gmail.com>
2022-07-06 08:13:16 +0300

    Updates

---
+++ pages/internship.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function internship() {
+  return <div>internship</div>;
+}
+
+export default internship;
---
+++ pages/investment.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function investment() {
+  return <div>investment</div>;
+}
+
+export default investment;
