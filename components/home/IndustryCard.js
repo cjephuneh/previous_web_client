8e61515eab00b4d8e2d700fb03edc3f4b36493f5
Beckham 45 <o.becks45@gmail.com>
2022-07-06 07:57:00 +0300

    Updates

--- components/home/IndustryCard.js
+++ components/home/IndustryCard.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function IndustryCard() {
   return (
-    <div>
-      <p>Indu</p>
+    <div className="divide-y divide-gray-400">
+      <div className=" py-8 w-3/4 md:w-1/2">
+        <h1 className="font-serif text-4xl md:text-6xl p-4">
+          Building for all{" "}
+          <span className="underline decoration-black decoration-4">
+            Industries
+          </span>
+        </h1>
+      </div>
     </div>
   );
 }
